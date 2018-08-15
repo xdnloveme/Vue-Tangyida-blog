47afe6317eb76bd07f276ccca9424753c42fe511
xdnloveme <530063113@qq.com>
2018-08-15 09:18:39 +0800

    #repair configuration -bug

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true,
+  },
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    // 'plugin:vue/essential',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard',
+    'plugin:vue/base'
+  ],
+  // // required to lint *.vue files
+  plugins: [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'generator-star-spacing': 'off',
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-tabs': 'off',
+    'no-useless-escape': 'off',
+    'eqeqeq': 'off',
+    'no-undef': 'off',
+    'indent': [0,2],
+    'camelcase': 'off',
+    'no-unused-vars': 'off',
+    "semi": [0, "always"],
+  }
+}
